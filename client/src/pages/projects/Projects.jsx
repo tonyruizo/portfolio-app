31c463dbe57281b8bff351d85d68772736525cc4
Tony Ruiz <tonyruizo@pm.me>
2025-04-03 15:00:32 -0400

    test

--- client/src/pages/projects/Projects.jsx
+++ client/src/pages/projects/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
     <>
       <h1 className="p-6">Coming soon!</h1>
 
-      <div>
+      <div className="py-6">
         <Link
           className="flex items-center justify-center space-x-3 bg-gray-800 hover:bg-gray-700 transition rounded-xl py-3 px-4 font-semibold shadow"
           to="/"
